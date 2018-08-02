ffb8124a770ab592bb35d2654ab7234f09bd3c4e
nikhilisnice <nikhilisnice@gmail.com>
2018-08-02 14:49:18 -0700

    First commit

---
+++ citiesOut_GeoJSONP.js
@@ -0,0 +1,113 @@
+eqfeed_callback({"type":"FeatureCollection","features":
+[
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.46372",
+        "1.49129"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.54277",
+        "1.73361"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.55623",
+        "1.53319"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.50729",
+        "1.53414"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.54499",
+        "1.51483"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.53474",
+        "1.58014"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.5676",
+        "1.59756"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.57205",
+        "1.48453"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "42.50779",
+        "1.52109"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "25.56473",
+        "55.55517"
+      ]
+    }
+  },
+  {
+    "alert": "green",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        "25.78953",
+        "55.9432"
+      ]
+    }
+  }
+]});
